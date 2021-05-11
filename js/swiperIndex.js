2f070c01ca62399131a9ba5230055a1aa3196680
Ophélie HORNN <hornn.ophelie@gmail.com>
2021-05-11 22:09:17 +0200

    slider modifié

--- js/swiperIndex.js
+++ js/swiperIndex.js
@@ -2,7 +2,16 @@ const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
+    breakpoints: {
+      800: {
+        slidesPerView: 2,
+        spaceBetween: 0
+      },
+      1100: {
+        slidesPerView: 3,
+        spaceBetween: 0
+      }
+    },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
